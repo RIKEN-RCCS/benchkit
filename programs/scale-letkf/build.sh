776fe1a1752d8ec00e9cf832f8b36cbf8c5876eb
Tsuyoshi Yamaura <tyamaura@riken.jp>
2026-01-08 00:43:24 +0900

    add a benchmark test: SCALE-LETKF

---
+++ programs/scale-letkf/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+system="$1"
+TOPDIR=`pwd`
+mkdir -p artifacts
+
+git clone https://github.com/SCALE-LETKF-RIKEN/scale-letkf-FugakuNEXT
+cd scale-letkf-FugakuNEXT
+
+case "$system" in
+    Fugaku)
+  source setup-env.FUGAKU.sh
+  cd scale/scale-rm/src
+  make -j
+  cd ../../scale-letkf/scale
+  make
+  cd $TOPDIR
+	;;
+    RC_GH200)
+  source setup-env.GH200.sh
+  cd scale/scale-rm/src
+  make -j
+  cd ../../scale-letkf/scale
+  make
+  cd $TOPDIR
+	;;
+    *)
+	echo "Unknown system: $system"
+	exit 1
+	;;
+esac
+
+echo "Storing executables and related artifacts for subsequent CI/CD jobs."
+mv scale-letkf-FugakuNEXT artifacts
