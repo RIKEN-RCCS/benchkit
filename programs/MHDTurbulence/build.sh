7cd3cef613d484f510928242bd288b2e68b47891
Tomoya Takiwaki <takiwaki.tomoya.astro@gmail.com>
2026-05-23 14:53:46 +0900

    Add MHDTurbulence benchmark
    Signed-off-by: Tomoya Takiwaki <takiwaki.tomoya.astro@gmail.com>

---
+++ programs/MHDTurbulence/build.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# usage: bash programs/MHDTurbulence/build.sh system
+
+set -e
+system="$1"
+
+code=MHDTurbulence
+REPO=https://github.com/cfcanaoj/MHDTurbulence.git
+artdir=artifacts
+
+echo "[${code}] Building on system: $system"
+
+mkdir -p ${artdir}
+
+if [ ! -d "$code" ]; then
+    git clone $REPO
+fi
+
+DIR=$code
+
+cd $DIR
+BIN=Simulation.x
+case "$system" in
+    MiyabiC)
+	cd src_f90_omp_host
+	echo "Compile cods in "`pwd`
+	make
+	echo "Executable is "${BIN}" and copied to "${artdir} 
+	cp ../exe/$BIN ../../${artdir}
+	;;
+    MiyabiG) 
+	cd src_f90_acc_device
+	echo "Compile cods in "`pwd`
+	make
+	echo "Executable is "${BIN}" and copied to "${artdir} 
+	cp ../exe/$BIN ../../${artdir}
+	;;
+# in the future, we may add this
+#    MiyabiG/OpenMP) 
+#	cd src_f90_omp_device
+#	echo "Compile cods in "`pwd`
+#	make
+#	echo "Executable is "${BIN}" and copied to "${artdir} 
+#	cp ../exe/$BIN ../../${artdir}
+#	;;
+    *)
+	echo "Unknown system: $system"
+	exit 1
+	;;
+esac
+
+echo "Storing executables and related artifacts for subsequent CI/CD jobs."
---
+++ programs/MHDTurbulence/run.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+# usage: bash scripts/test_submit.sh MHDTurbulence n
+
+set -eux
+system="$1"
+nodes="$2"
+numproc_node="$3"
+nthreads="$4"
+
+code=MHDTurbulence
+artdir=artifacts
+BIN=Simulation.x
+
+ROOT=$(cd "$(dirname "$0")/../.." && pwd)
+mkdir -p results
+
+LOG=stdout.log
+case "$system" in
+    MiyabiC)
+	exedir=exe
+	mkdir -p $code/$exedir/
+	cp $artdir/${BIN} $code/$exedir/
+	cd $code/$exedir/
+	echo "code is executed in "$code/$exedir/
+	NP=$((nodes * numproc_node))
+	export OMP_NUM_THREADS=$nthreads
+	export KMP_AFFINITY=compact
+	mpirun -np "$NP" ./Simulation.x > ${LOG} 2>&1
+	elapsed=$(grep " sim time \[s\]:    " ${LOG} | awk '{print $4}' | tail -n 1)
+	tcc=$(grep " time/count/cell" ${LOG} | awk '{print $2}' | tail -n 1)
+	;;
+    MiyabiG)
+	exedir=exe
+	mkdir -p $code/$exedir/
+	cp $artdir/${BIN} $code/$exedir/
+	cd $code/$exedir/
+	echo "code is executed in "$code/$exedir/
+	NP=$((nodes * numproc_node))
+	mpirun -np "$NP" ./Simulation.x > ${LOG} 2>&1
+	elapsed=$(grep " sim time \[s\]:    " ${LOG} | awk '{print $4}' | tail -n 1)
+	tcc=$(grep " time/count/cell" ${LOG} | awk '{print $2}' | tail -n 1)
+	;;
+    *)
+	echo "Unknown system: $system"
+	exit 1
+	;;
+esac
+
+
+if [ -z "${elapsed}" ]; then
+  echo "ERROR: elapsed time was not found in ${LOG}"
+  exit 1
+fi
+
+if [ -z "${tcc}" ]; then
+  echo "ERROR: time/count/cell was not found in ${LOG}"
+  exit 1
+fi
+
+cd ${ROOT}
+source "${ROOT}/scripts/bk_functions.sh"
+
+fomtcc=$(awk "BEGIN {printf \"%.6e\", 1.0/$tcc}")
+
+
+########################################
+# emit benchkit result
+########################################
+{
+  bk_emit_result \
+    --fom "$fomtcc" \
+    --fom-version "cell_updates_per_sec" \
+    --exp "KH" \
+    --nodes "$nodes" \
+    --numproc-node "$numproc_node" \
+    --nthreads "$nthreads"
+
+  bk_emit_section total "$elapsed"
+} > "${ROOT}/results/result"
